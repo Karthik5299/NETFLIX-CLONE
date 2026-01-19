2aee394e9bf21ce08b6beb09488578e85f09dadc
Jami Karthikeya <152587305+Karthik5299@users.noreply.github.com>
2026-01-19 10:11:59 +0530

    Update Home.jsx

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import hero_banner from '../../assets/hero_banner.jpg'
+import hero_banner from '../../assets/hero_banner.png'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
@@ -16,10 +16,17 @@ const Home = () => {
       <div className="hero">
         <img src={hero_banner} alt="" className='banner-img'/>
         <div className="hero-caption">
-          <img src={hero_title} alt="" className='caption-img'/>
-          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                   <p>After years of slience,his enemies thought he'd gone for good -- but Ojas Gambheera(pawan kalyan ) is back , and he's out for revebge.</p>
           <div className="hero-btns">
-            <button className='btn'><img src={play_icon} alt="" />Play</button>
+  <a
+    href="https://www.youtube.com/watch?v=FbXOsVByKmk"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <button className="btn">
+      <img src={play_icon} alt="" /> Play
+    </button>
+  </a>
             <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
           </div>
           <TitleCards/>
